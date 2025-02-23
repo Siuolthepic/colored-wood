1040b47c71bd66a19ee5e03a05096f89c4f8f9ba
Siuolplex <lmeitrodt@gmail.com>
2025-02-23 13:37:59 -0600

    2.1.0 - Russian Translation + Programmer Art from Klayzer
    Removed the archtexture extensions stuff as well, will probably be the version that will get backported
    
    Signed-off-by: Siuolplex <lmeitrodt@gmail.com>

--- fabric/src/main/java/io/siuolplex/wood_you_dye/fabric/WoodYouDye.java
+++ fabric/src/main/java/io/siuolplex/wood_you_dye/fabric/WoodYouDye.java
@@ -5,6 +5,11 @@ import io.siuolplex.wood_you_dye.registry.WoodYouDyeItems;
 import io.siuolplex.wood_you_dye.util.Loader;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
@@ -20,6 +25,12 @@ public class WoodYouDye implements ModInitializer {
                 content.addBefore(Items.STONE.getDefaultInstance(), item.getDefaultInstance());
             }
         });
+
+        ResourceManagerHelper.registerBuiltinResourcePack(
+                ResourceLocation.fromNamespaceAndPath("wood_you_dye", "programmer_art"),
+                FabricLoader.getInstance().getModContainer("wood_you_dye").get(),
+                Component.literal("Wood you Dye Programmer Art"),
+                ResourcePackActivationType.NORMAL);
     }
 
     public static class WYDFabricLoader implements Loader {
--- neoforge/src/main/java/io/siuolplex/wood_you_dye/forge/WoodYouDye.java
+++ neoforge/src/main/java/io/siuolplex/wood_you_dye/forge/WoodYouDye.java
@@ -2,9 +2,16 @@ package io.siuolplex.wood_you_dye.forge;
 
 import io.siuolplex.wood_you_dye.WoodYouDyeMain;
 import io.siuolplex.wood_you_dye.util.Loader;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
 
 @Mod(WoodYouDye.ID)
 public class WoodYouDye {
@@ -12,12 +19,12 @@ public class WoodYouDye {
 
     public static final Loader MODLOADER = new ForgeLoader();
 
-
     final ModLoadingContext modLoadingContext = ModLoadingContext.get();
     final IEventBus modEventBus = modLoadingContext.getActiveContainer().getEventBus();
 
     public WoodYouDye() {
         WoodYouDyeMain.init(MODLOADER);
+        modEventBus.register(this);
     }
 
     public static final class ForgeLoader implements Loader {
@@ -25,6 +32,17 @@ public class WoodYouDye {
         public String getName() {
             return "neoforge";
         }
+    }
+
+    @SubscribeEvent
+    public void addProgArtPack(AddPackFindersEvent event) {
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath("wood_you_dye", "resourcepacks/programmer_art"),
+                PackType.CLIENT_RESOURCES,
+                Component.literal("Wood You Dye Programmer Art"),
+                PackSource.DEFAULT,
+                false,
+                Pack.Position.TOP);
 
     }
 }
